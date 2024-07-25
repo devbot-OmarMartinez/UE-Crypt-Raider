5a2bd7676e27f8ea84faa90d9951a6095ff9be66
Omar Martinez <fmartinez@devbot.com>
2024-07-25 09:39:26 -0600

    Applying Damage

--- Source/ToonTanks/BasePawn.cpp
+++ Source/ToonTanks/BasePawn.cpp
@@ -44,4 +44,5 @@ void ABasePawn::Fire()
 	FRotator Rotation = ProjectileSpawnPoint->GetComponentRotation();
 
 	auto Projectile = GetWorld()->SpawnActor<AProjectile>(ProjectileClass, Location, Rotation);
+	Projectile->SetOwner(this);
 }
